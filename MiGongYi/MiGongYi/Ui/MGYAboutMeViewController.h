a0d59e5132f4f625aaa687b5db2d49a7364d46f5
ozr <ozr@ricedonate.com>
2014-09-25 09:38:12 +0800

    commit

---
+++ MiGongYi/MiGongYi/Ui/MGYAboutMeViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutMeViewController.h
+//  MiGongYi
+//
+//  Created by megil on 9/19/14.
+//  Copyright (c) 2014 megil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MGYBaseViewController.h"
+#import "AboutMeTableViewCell.h"
+
+@interface MGYAboutMeViewController : MGYBaseViewController<UITableViewDelegate, UITableViewDataSource, MGYAboutMeTableViewCellDelegate>
+
+@end
